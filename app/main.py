7f83fd7eaa28d8b0e4c8acbc17b8f0f7d62e338d
FelicianoRomero <felicianoromero.sec@institutolamerced.edu.ar>
2023-11-12 11:16:02 -0300

    Example T&C pop-up

--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
 from kivy.app import App
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.popup import Popup
+from kivy.clock import Clock
+
 
 USER = 'admin'
 PWD = '1234'
+
+class CustomPopup(Popup):
+    pass
 
 class Funcs(Screen):
     pass
 
 class MainMenu(Screen):
-    pass
+    def on_enter(self):
+        Clock.schedule_once(self.pop)
+
+    def pop(self, dt):
+        popup = CustomPopup()
+        popup.open()
+            
 
 class Login(Screen):
 
@@ -18,8 +30,6 @@ class Login(Screen):
         in_pwd = self.ids.pwd.text
         if in_user == USER and in_pwd == PWD:
             App.get_running_app().root.current = "funcs"
-
-
         else:
             self.ids.warning.text = "Usuario o Contraseña INCORRECTOS\nIntente ingresando los datos nuevamente"
     def test(self):
