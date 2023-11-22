74f42838d4e2e3615d7a24e1e9f2132804d1d77d
FelicianoRomero <felicianoromero.sec@institutolamerced.edu.ar>
2023-11-22 09:33:33 -0300

    Implementacion de Base de Datos

--- server/server.py
+++ server/server.py
@@ -1,14 +1,55 @@
 from flask import Flask
+import sqlite3 as sql
+from flask import request
+
+
+class SQLdb:
+    def __init__(self) -> None:
+        pass
+
+    def con(self):
+        con = sql.connect('test.db')
+        cur = con.cursor()
+        return con, cur
+
+    def create_tbls(self):
+        con, cur = self.con()
+        cur.execute('''CREATE TABLE IF NOT EXISTS test (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT    
+        )''')
+
+    def post(self, name):
+        con, cur = self.con()
+        cur.execute('INSERT INTO test (name) VALUES (?)', (name,))
+        con.commit()
+        cur.close()
+        return 'Success'
+
+    def get(self, x):
+        con, cur = self.con()
+        cur.execute('SELECT name FROM test WHERE id = ?', (x, ))
+        data = cur.fetchall()[0][0]
+        print(data)
+        return data
 
 class Listener:
     def __init__(self):
         self.app = Flask(__name__)
+        self.db = SQLdb()
 
         @self.app.route('/', methods=["GET"])
         def home():
             return 'Hi'
+        
+        @self.app.route('/test', methods=["POST"])
+        def receive():
+            data = request.get_json()
+            x = data.get('id')
+            return self.db.get(x)
 
     def run(self):
+        self.db.create_tbls()
         self.app.run(host="0.0.0.0", port=8080, debug=True)
 
 if __name__ == '__main__':
---
+++ tester.py
@@ -0,0 +1,22 @@
+import requests
+
+def send_request():
+    target_ip = "192.168.88.78:8080"
+    url = f'http://{target_ip}/test'
+    json_data = {'id': 3, 'name': 'Lauti Puto'} 
+
+    try:
+        # Realiza la solicitud POST a la dirección IP especificada
+        response = requests.post(url, json=json_data)
+
+        # Verifica si la solicitud fue exitosa (código de estado 2xx)
+        if response.ok:
+            print(response.text)
+        else:
+            print(f'Error en la solicitud. Código de estado: {response.status_code}')
+
+    except requests.exceptions.RequestException as e:
+        print(f'Error durante la solicitud: {e}')
+
+if __name__ == '__main__':
+    send_request()
